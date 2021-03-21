0bed921b9561413cf67a59ff89664b1530e427b2
Eduardo Souza <eduardosouzv@gmail.com>
2021-03-21 00:46:47 -0300

    token var

--- index.js
+++ index.js
@@ -5,28 +5,27 @@ const fs = require('fs');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
 
-for (const file of commandFiles){
-  const command = require(`./commands/${file}`)
-  
-  client.commands.set(command.name, command)
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.name, command);
 }
 
 client.once('ready', () => {
-  client.user.setActivity('EARFQUAKE', {type: 'LISTENING' })
+  client.user.setActivity('EARFQUAKE', { type: 'LISTENING' });
   console.log('prontalhos mete ficha');
 });
 
-client.on('message', message => {
+client.on('message', (message) => {
   if (!client.commands.has(message.content) || message.author.bot) return;
-  
+
   try {
     client.commands.get(message.content).execute(message);
   } catch (error) {
     console.error(error);
   }
-
 });
 
-client.login(token);
+client.login(process.env.TOKEN);
