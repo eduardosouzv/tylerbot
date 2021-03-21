ffa3ea5f74c9d069fe26c7ff4c127cfa09b613ce
Eduardo Souza <eduardosouzv@gmail.com>
2021-03-20 22:14:40 -0300

    command to get class link working

---
+++ commands/aula.js
@@ -0,0 +1,242 @@
+const dayjs = require('dayjs');
+const getSchedule = require('./getSchedule');
+
+const { prefix } = require('../config.json');
+
+var isBetween = require('dayjs/plugin/isBetween');
+var customParseFormat = require('dayjs/plugin/customParseFormat');
+
+dayjs.extend(isBetween);
+dayjs.extend(customParseFormat);
+
+const data = {
+  weekDay: '',
+  classLink: '',
+  numClass: '',
+};
+
+const class1 = dayjs('07:10', 'HH:mm');
+const class2 = dayjs('08:00', 'HH:mm');
+const class3 = dayjs('08:50', 'HH:mm');
+const class4 = dayjs('09:55', 'HH:mm');
+const class5 = dayjs('10:45', 'HH:mm');
+const class6 = dayjs('11:35', 'HH:mm');
+const classEnd = dayjs('12:30', 'HH:mm');
+
+let res;
+
+getSchedule().then((response) => {
+  res = response;
+});
+
+function a() {
+  let nowDate = new Date('January 5, 2021 07:24:00');
+  switch (dayjs(nowDate).day()) {
+    case 1: //segunda
+      data.weekDay = 'Segunda-Feira';
+
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class1, class2, 'minute', '[)')) {
+        // 1 aula
+        data.classLink = res.segunda.aula1;
+        data.numClass = '1';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class2, class3, 'minute', '[)')) {
+        // 2 aula
+        data.classLink = res.segunda.aula2;
+        data.numClass = '2';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class3, class4, 'minute', '[)')) {
+        // 3 aula
+        data.classLink = res.segunda.aula3;
+        data.numClass = '3';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class4, class5, 'minute', '[)')) {
+        // 4 aula
+        data.classLink = res.segunda.aula4;
+        data.numClass = '4';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class5, class6, 'minute', '[)')) {
+        // 5 aula
+        data.classLink = res.segunda.aula5;
+        data.numClass = '5';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class6, classEnd, 'minute', '[)')) {
+        // 6 aula
+        data.classLink = res.segunda.aula6;
+        data.numClass = '6';
+        break;
+      }
+
+    case 2: // terca
+      data.weekDay = 'Terça-Feira';
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class1, class2, 'minute', '[)')) {
+        // 1 aula
+        data.classLink = res.terca.aula1;
+        data.numClass = '1';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class2, class3, 'minute', '[)')) {
+        // 2 aula
+        data.classLink = res.terca.aula2;
+        data.numClass = '2';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class3, class4, 'minute', '[)')) {
+        // 3 aula
+        data.classLink = res.terca.aula3;
+        data.numClass = '3';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class4, class5, 'minute', '[)')) {
+        // 4 aula
+        data.classLink = res.terca.aula4;
+        data.numClass = '4';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class5, class6, 'minute', '[)')) {
+        // 5 aula
+        data.classLink = res.terca.aula5;
+        data.numClass = '5';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class6, classEnd, 'minute', '[)')) {
+        // 6 aula
+        data.classLink = res.terca.aula6;
+        data.numClass = '6';
+        break;
+      }
+    case 3: // quarta
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class1, class2, 'minute', '[)')) {
+        // 1 aula
+        data.classLink = res.quarta.aula1;
+        data.numClass = '1';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class2, class3, 'minute', '[)')) {
+        // 2 aula
+        data.classLink = res.quarta.aula2;
+        data.numClass = '2';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class3, class4, 'minute', '[)')) {
+        // 3 aula
+        data.classLink = res.quarta.aula3;
+        data.numClass = '3';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class4, class5, 'minute', '[)')) {
+        // 4 aula
+        data.classLink = res.quarta.aula4;
+        data.numClass = '4';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class5, class6, 'minute', '[)')) {
+        // 5 aula
+        data.classLink = res.quarta.aula5;
+        data.numClass = '5';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class6, classEnd, 'minute', '[)')) {
+        // 6 aula
+        data.classLink = res.quarta.aula6;
+        data.numClass = '6';
+        break;
+      }
+    case 4: // quinta
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class1, class2, 'minute', '[)')) {
+        // 1 aula
+        data.classLink = res.quinta.aula1;
+        data.numClass = '1';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class2, class3, 'minute', '[)')) {
+        // 2 aula
+        data.classLink = res.quinta.aula2;
+        data.numClass = '2';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class3, class4, 'minute', '[)')) {
+        // 3 aula
+        data.classLink = res.quinta.aula3;
+        data.numClass = '3';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class4, class5, 'minute', '[)')) {
+        // 4 aula
+        data.classLink = res.quinta.aula4;
+        data.numClass = '4';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class5, class6, 'minute', '[)')) {
+        // 5 aula
+        data.classLink = res.quinta.aula5;
+        data.numClass = '5';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class6, classEnd, 'minute', '[)')) {
+        // 6 aula
+        data.classLink = res.quinta.aula6;
+        data.numClass = '6';
+        break;
+      }
+
+    case 5: // sexta
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class1, class2, 'minute', '[)')) {
+        // 1 aula
+        data.classLink = res.sexta.aula1;
+        data.numClass = '1';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class2, class3, 'minute', '[)')) {
+        // 2 aula
+        data.classLink = res.sexta.aula2;
+        data.numClass = '2';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class3, class4, 'minute', '[)')) {
+        // 3 aula
+        data.classLink = res.sexta.aula3;
+        data.numClass = '3';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class4, class5, 'minute', '[)')) {
+        // 4 aula
+        data.classLink = res.sexta.aula4;
+        data.numClass = '4';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class5, class6, 'minute', '[)')) {
+        // 5 aula
+        data.classLink = res.sexta.aula5;
+        data.numClass = '5';
+        break;
+      }
+      if (dayjs(nowDate.toTimeString(), 'HH:mm').isBetween(class6, classEnd, 'minute', '[)')) {
+        // 6 aula
+        data.classLink = res.sexta.aula6;
+        data.numClass = '6';
+        break;
+      }
+    default:
+      // 😎🍺
+      break;
+  }
+}
+module.exports = {
+  name: `${prefix}aula`,
+  execute(message) {
+    a();
+    message.channel.send({
+      embed: {
+        color: '#FF0000',
+        title: `👺 **${data.weekDay}** | Aula ${data.numClass}`,
+        description: `\n**Link:   ** ${data.classLink}`,
+      },
+    });
+  },
+};
---
+++ commands/getSchedule.js
@@ -0,0 +1,68 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+async function getSchedule() {
+  try {
+    const result = await axios.get(
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQgsu8aH0A6edj2xWspyynCgimYnHsYNcw4KpfoRCEAT9WiD2Vi9ic3O1knYQi3k24UG9VKl0ZQi1ly/pubhtml?gid=975049268&single=true'
+    );
+
+    let $ = cheerio.load(result.data);
+
+    let messLinks = $('td > a').text();
+
+    let arrLinks = messLinks.split('meet');
+    let classLinks = [];
+
+    arrLinks.map(async (el) => {
+      classLinks.push(
+        'https://meet' + el.replace('https://', '').replace('?hs=122&authuser=0', '')
+      );
+    });
+    return {
+      segunda: {
+        aula1: classLinks[1],
+        aula2: classLinks[5],
+        aula3: classLinks[9],
+        aula4: classLinks[13],
+        aula5: classLinks[14],
+        aula6: 'esse ta bugado',
+      },
+      terca: {
+        aula1: classLinks[2],
+        aula2: classLinks[6],
+        aula3: classLinks[10],
+        aula4: classLinks[18],
+        aula5: classLinks[19],
+        aula6: classLinks[23],
+      },
+      quarta: {
+        aula1: classLinks[3],
+        aula2: 'edf',
+        aula3: 'edf',
+        aula4: classLinks[15],
+        aula5: classLinks[12],
+        aula6: classLinks[24],
+      },
+      quinta: {
+        aula1: classLinks[4],
+        aula2: classLinks[7],
+        aula3: classLinks[11],
+        aula4: classLinks[16],
+        aula5: classLinks[21],
+        aula6: classLinks[25],
+      },
+      sexta: {
+        aula1: 'esse ta bugado',
+        aula2: classLinks[8],
+        aula3: classLinks[12],
+        aula4: classLinks[17],
+        aula5: classLinks[22],
+      },
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = getSchedule;
